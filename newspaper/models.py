1f5f97c515d42d373535001cf05b2abaedb14b21
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2025-11-19 16:00:05 +0545

    Working on newsportal site

---
+++ newspaper/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+# Heading, author image, author, published date, views, category, tags, artical image, content, sub heading, comments, display comments
+
+# Create your models here.
+
+
+class TimeStampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract  = True # Don't create table in DB
+
+class Category(TimeStampModel):
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100, null=True, blank=True)
+    description =models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ["name"] # Category.objects.all()
+        verbose_name = "category"
+        verbose_name_plural = "Categories"
+
+class Tag(TimeStampModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class Post(TimeStampModel):
+    STATUS_CHOICES = [
+        ("active", "Active"),
+        ("in_active", "Inactive"),
+    ]
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    featured_image = models.ImageField(upload_to="post_image/%Y/%m/%d", blank=False)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="active")
+    view_count = models.PositiveBigIntegerField(default=0)
+    is_breaking_news = models.BooleanField(default=False)
+    published_at = models.DateTimeField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
---
+++ newspaper/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+
+from newspaper.models import Post
+
+from django.views.generic import TemplateView
+from django.utils import timezone
+from datetime import timedelta
+
+
+# Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = "newsportal/home.html"
+
+
+    # If we want to send extra data to the template we user get_context_data
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["breaking_news"] = Post.objects.filter(
+            published_at__isnull=False, status="active", is_breaking_news=True
+        ).order_by("-published_at")[:3]
+
+        context["featured_post"] = (
+            Post.objects.filter(published_at__isnull=False, status="active")
+            .order_by("-published_at", "-views_count")
+            .first()
+        )
+
+        context["trending_news"] = Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-published_at")[:4]
+
+        one_week_ago = timezone.now() - timedelta(days=7)
+        context["week_top_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active", published_at__gte=one_week_ago
+        ).order_by("-published_at", "-views_count")[:5]
+
+        context["popular_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-published_at")[:5]
+
+        return context
