4c4882da366f23600d91642d11ec33c305602985
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2025-11-21 15:30:29 +0545

    Added Latest News click able and updated most_popular.html file

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomeView
+from . import views
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
+    path("", views.HomeView.as_view(), name="home"),
+    path("post-list/", views.PostListView.as_view(), name="post-list"),
 ]
--- newspaper/views.py
+++ newspaper/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from newspaper.models import Post
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.utils import timezone
 from datetime import timedelta
 
@@ -42,4 +42,24 @@ class HomeView(TemplateView):
             published_at__isnull=False, status="active"
         ).order_by("-published_at")[:5]
 
+        return context
+    
+
+class PostListView(ListView):
+    model = Post
+    template_name = "newsportal/list/list.html"
+    context_object_name = "posts"
+    paginate_by = 1
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-published_at")
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["popular_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-published_at")[:5]
+        
         return context
