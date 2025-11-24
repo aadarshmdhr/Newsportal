19602cacad03ebce1c6dcfb5343f45482c628455
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2025-11-24 15:11:36 +0545

    Added contact in models.py

--- newspaper/models.py
+++ newspaper/models.py
@@ -58,3 +58,15 @@ class Advertisement(TimeStampModel):
 
     def __str__(self):
         return self.title
+
+class Contact(TimeStampModel):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ["created_at"]
