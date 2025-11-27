9cd22a93937c21af0fc7b83337790597632bfe7f
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2025-11-27 15:29:51 +0545

    Adding comment section in models.py

--- newspaper/models.py
+++ newspaper/models.py
@@ -79,3 +79,11 @@ class OurTeam(TimeStampModel):
 
     def __str__(self):
         return self.name
+    
+class Comment(TimeStampModel):
+    user = models.ForeignKey("auth.user", on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.user} - {self.post}"
