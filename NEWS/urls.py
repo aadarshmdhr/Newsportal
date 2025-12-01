9e822c98be5821f4c58de86cd2be55332ea5c2d7
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2025-12-01 14:56:53 +0545

    Added editing bar in admin panel

--- NEWS/urls.py
+++ NEWS/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
     path("", include("newspaper.urls")),
     path("accounts/", include("accounts.urls")),
     path("reports/", include("reports.urls")),
--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
+from django.contrib import admin
+from django import forms
+from tinymce.widgets import TinyMCE
+from .models import Post
+
 from newspaper.models import Category, Post, Tag, Advertisement, Contact, OurTeam, Comment, UserProfile, Newsletter
 
 # Register your models here.
@@ -7,10 +12,27 @@ from newspaper.models import Category, Post, Tag, Advertisement, Contact, OurTea
 
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Post)
+# admin.site.register(Post)
 admin.site.register(Advertisement)
 admin.site.register(Contact)
 admin.site.register(OurTeam)
 admin.site.register(Comment)
 admin.site.register(UserProfile)
 admin.site.register(Newsletter)
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+    list_display = ('title', 'author', 'status', 'published_at')
+    list_filter = ('status', 'category', 'author')
+    search_fields = ('title', 'content')
+
+admin.site.register(Post, PostAdmin)
