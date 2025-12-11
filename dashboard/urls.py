479deda79ef9203c141c06c23e8ce3ff58eaf6c9
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2025-12-12 00:03:06 +0545

    Added side bar to view post

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.AdminPostListView.as_view(), name="post-list"),
     path("post-create/", views.AdminPostCreateView.as_view(), name="post-create"),
     path("post-delete/<int:pk>/", views.AdminPostDeleteView.as_view(), name="post-delete"),
+    path("post-update/<int:pk>/", views.AdminPostUpdateView.as_view(), name="post-update"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, View
+from django.views.generic import ListView, CreateView, View, UpdateView
 
 from django.views.generic import TemplateView
 
 from dashboard.forms import PostForm
 from newspaper.models import Post
 
+from django.contrib.auth.mixins import UserPassesTestMixin
+
 # Create your views here.
 
-class AdminPostListView(ListView):
+class StaffRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_staff
+    
+    def handle_no_permission(self):
+        return redirect("/")
+
+class AdminPostListView(StaffRequiredMixin, ListView):
     model = Post
     template_name = "dashboard/post_list.html"
     context_object_name = "posts"
@@ -18,7 +27,7 @@ class AdminPostListView(ListView):
         return Post.objects.all().order_by("-published_at")
     
 
-class AdminPostCreateView(CreateView):
+class AdminPostCreateView(StaffRequiredMixin, CreateView):
     model = Post
     template_name = "dashboard/post_create.html"
     form_class = PostForm
@@ -29,8 +38,14 @@ class AdminPostCreateView(CreateView):
         return super().form_valid(form)
     
     
-class AdminPostDeleteView(View):
+class AdminPostDeleteView(StaffRequiredMixin, View):
     def get(self, request, pk):
         post = Post.objects.get(pk=pk)
         post.delete()
         return redirect("dashboard:post-list")
+    
+class AdminPostUpdateView(StaffRequiredMixin, UpdateView):
+    model = Post
+    template_name = "dashboard/post_create.html"
+    form_class = PostForm
+    success_url = reverse_lazy("dashboard:post-list")
